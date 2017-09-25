54f35b636b3476ededa64258fd1530c014e4598b
Kevin <k.mcginley@nuigalway.ie>
2017-09-25 18:51:49 +0100

    question 1

--- src/q1/assignment.c
+++ src/q1/assignment.c
@@ -2,5 +2,19 @@
 
 int main(int arg, char* argc[]){
   printf("Hello assignment1.\n");
+
+  int a = 1;
+  int* b = &a;
+  long c = 3;
+  double d = 5.2;
+  double* e = &d;
+  char* f = "letters";
+  char** g = &f;
+
+  printf("int size: %lu\n",sizeof(a));
+  printf("int* size: %lu\n",sizeof(b));
+  printf("long size: %lu\n",sizeof(c));
+  printf("double* size: %lu\n",sizeof(e));
+  printf("char** size: %lu\n",sizeof(g));
   return 0;
 }
