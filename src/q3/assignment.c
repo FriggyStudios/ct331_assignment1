7ff3b8f0c2d1aa37430cdbe8d8bcb40ba7baa532
Kevin <k.mcginley@nuigalway.ie>
2017-09-28 23:00:40 +0100

    More Question3

--- src/q3/assignment.c
+++ src/q3/assignment.c
@@ -3,21 +3,41 @@
 #include "tests.h"
 
 void printChar(void* data);
+void printInt(void* data);
+void printFloat(void* data);
 
 void printChar(void* data){
   printf("%s\n",(char*)(data));
 }
-
 void printInt(void* data){
   printf("%d\n",*((int*)data));
 }
+void printFloat(void* data){
+  printf("%f\n",*((float*)data));
+}
+
 int main(int arg, char* argc[]){
-  
+  printf("Create List\n");  
   listElement* l = createEl("First Element", 30, printChar);
   traverse(l);
+  printf("Length: %d\n",length(l));
+
+  printf("\nInsert to List\n");
   int two = 2;
   insertAfter(l,&two, 10, printInt);
   traverse(l);
+  printf("Length: %d\n",length(l));
+ 
+  printf("\nPush to List\n");
+  float three = 3.14f;
+  push(&l,&three,10,printFloat);
+  traverse(l);
+  printf("Length: %d\n",length(l));
+ 
+  printf("\nPop from  List\n");
+  pop(&l);
+  traverse(l); 
+  printf("Length: %d\n",length(l));
 
   runTests();
   return 0;
